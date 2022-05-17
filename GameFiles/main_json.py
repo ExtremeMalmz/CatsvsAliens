6781f069f151c7e4b60752a96ec32722d1c875ae
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-05-17 10:28:12 +0200

    SAYED HA DETTA
    Adsada

---
+++ GameFiles/main_json.py
@@ -0,0 +1,27 @@
+import json, os
+
+def create_json():
+    '''
+    Creates a JSON file named games.json if it doesn't exist already. 
+    '''
+    try:
+        # Opens the file "gemes.json" in "read" mode
+        my_file = open(os.path.join('Assets','games.json'), 'r')
+        # Reads and converts the file content (JSON) to Python datatype (list)
+        game = json.loads(my_file.read())
+        # Closes the file
+        my_file.close()
+        # Returns the list of every lines in file
+        return game
+
+    except FileNotFoundError:
+        data = {"Games": [{"Player_Name": "Boy Wonder", "Player_Level" : 0, "Player_Life_amount" : 9}]}
+
+        # Creates a new file called "gemes.json"
+        my_file = open(os.path.join('Assets','games.json'), 'w')
+        # Writes the basic structure in JSON-format
+        my_file.write(json.dumps(data, indent = 4))
+        my_file.close()
+        # Returns the basic structure as an empty list
+        return {}
+
--- README.txt
+++ README.txt
@@ -5,6 +5,8 @@ Ni måste ladda ner Pygame och Pygame_Menu ramverket för att spela spelet.
 
 Detta gör ni med att ni anger "pip install pygame" och "pip install pygame_menu" i eran konsol på VisualStudioCode.
 
+Installera också ffpyplayer,pymediainfo        
+
 Bakgrunds musiken har tagits bort pga att det tar för mycket plats på Sallys Gruppsida. 
 
 Regards, Management
