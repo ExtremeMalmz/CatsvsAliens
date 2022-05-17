de1756642a1004aea2a947b246c8af0c0f46c75d
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-05-17 14:28:50 +0200

    Added everything
    demo 2.1

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from main_menu import the_main
+from GameFiles.main_menu import the_main
+from GameFiles.main_json import create_json
 
-import json
-
-def create_json():
-    '''
-    Creates a JSON file named games.json if it doesn't exist already. 
-    '''
-    try:
-        # Opens the file "gemes.json" in "read" mode
-        my_file = open("games.json", "r")
-        # Reads and converts the file content (JSON) to Python datatype (list)
-        game = json.loads(my_file.read())
-        # Closes the file
-        my_file.close()
-        # Returns the list of every lines in file
-        return game
-
-    except FileNotFoundError:
-        # Creates a new file called "gemes.json"
-        my_file = open("games.json", "w")
-        # Writes the basic structure in JSON-format
-        my_file.write(json.dumps([" Boy Wonder"]))
-        my_file.close()
-        # Returns the basic structure as an empty list
-        return []
-
+from Levels.level4 import level_4_game_loop
 
 if __name__ == '__main__':
     '''
