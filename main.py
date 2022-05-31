22a679d5b17957ea17ff902ce0fd03a328fd493b
AnitaMjaow <olsson_anita@live.se>
2022-05-31 03:06:01 +0200

    changes in main file

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-from GameFiles.main_menu import the_main
-from GameFiles.main_json import create_json
+import os
+from main_menu import the_main
+
+import json
+import os
+
+
+def create_json():
+    '''
+    Creates a JSON file named games.json if it doesn't exist already. 
+    '''
+    try:
+        # Opens the file "gemes.json" in "read" mode
+        my_file = open(os.path.join('Assets', 'games.json'), 'r')
+        # Reads and converts the file content (JSON) to Python datatype (list)
+        game = json.loads(my_file.read())
+        # Closes the file
+        my_file.close()
+        # Returns the list of every lines in file
+        return game
+
+    except FileNotFoundError:
+        # Creates a new file called "gemes.json"
+        my_file = open(os.path.join('Assets', 'games.json'), 'w')
+        # Writes the basic structure in JSON-format
+        my_file.write(json.dumps([" Boy Wonder"]))
+        my_file.close()
+        # Returns the basic structure as an empty list
+        return []
+
 
 if __name__ == '__main__':
     '''
