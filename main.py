b614882ac12e2fa013e226de5cf3b38adca16df5
AnitaMjaow <olsson_anita@live.se>
2022-05-31 03:26:28 +0200

    added files

---
+++ main.py
@@ -0,0 +1,9 @@
+from GameFiles.main_menu import the_main
+from GameFiles.main_json import create_json
+
+if __name__ == '__main__':
+    '''
+    if the name of the file is main.py it will run, otherwise not
+    '''
+    create_json()
+    the_main() 
